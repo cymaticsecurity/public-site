3d90c61ab559c7d23960d73a90376bdc688931e3
narcisso <44299423+narcisso@users.noreply.github.com>
2021-03-23 17:51:47 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,9 @@
-CymaticXid.init({});
+let attempts = 10;
+let timer = setInterval(function() {
+  if(!attempts <= 0){ return clearInterval(timer); } // Give up
+  attempts -=1;
+  if(document.querySelector('script[src^="https://sdk.cymatic.io"]')){
+    CymaticXid.init({});
+    clearInterval(timer);
+  }
+}, 300);
