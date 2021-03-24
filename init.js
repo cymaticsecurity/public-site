7031b25df09658bd00607c3f64b583695ee68f10
narcisso <44299423+narcisso@users.noreply.github.com>
2021-03-23 18:23:43 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,3 +1,16 @@
 setTimeout(function(){
- CymaticXid.init({});
+ let form = document.querySelector('form[data-customer-information-form]');
+ 
+ if(form){
+   CymaticXid.v2.init({
+    checkout : {
+      selector : 'form[data-customer-information-form]',
+      username : 'input#checkout_email_or_phone',
+      password : 'input#checkout_shipping_address_last_name',
+      submit   : 'button#continue_button'
+    }
+  });
+ } else {
+  CymaticXid.init();
+ }
 }, 500);
