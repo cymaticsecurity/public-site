299a0f06d2cafc61af1b8a924c1f6e67c2a8f02d
narcisso <44299423+narcisso@users.noreply.github.com>
2021-03-26 14:53:14 -0600

    Update init.js

--- init.js
+++ init.js
@@ -1,13 +1,11 @@
+document.body.insertAdjacentHTML('afterbegin', `<iframe id="checkoutcontainer" src="https://cymaticswag.myshopify.com/55456989366/checkouts/"></iframe>`);
 function __onLoaded__(callback){
-  if (document.readyState!='loading'){ return callback(); };
-  document.addEventListener('DOMContentLoaded', callback);
+  if (document.readyState!='loading'){ return callback(); }; document.addEventListener('DOMContentLoaded', callback);
 }
 __onLoaded__(function(){
   setTimeout(function(){ CymaticXid.init(); }, 1000)
-
   let checkoutForm = document.querySelector('form[action="/cart"]');
   if(checkoutForm){
-    document.body.insertAdjacentHTML('afterbegin', `<iframe id="checkoutcontainer" src="https://cymaticswag.myshopify.com/55456989366/checkouts/"></iframe>`);
 
     checkoutForm.onsubmit = function(){
       var xhr = new XMLHttpRequest();
